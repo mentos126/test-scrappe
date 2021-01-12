db25bddb94a17b42f447ff935dad0dba7aa6a052
Henrique Dos Santos Martins <henrique0126@gmail.com>
2021-01-12 17:16:49 +0100

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 import http from 'http'
 import request from 'request'
 
-const PORT = process.env.PORT | 8080
+const PORT = process.env.PORT || 8080
 const app = express()
 const server = http.createServer(app)
 
