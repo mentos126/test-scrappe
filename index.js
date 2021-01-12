8d0cec9ac9933956e9b99b49c9ff36a18c0546cf
Henrique Dos Santos Martins <henrique0126@gmail.com>
2021-01-12 16:30:42 +0100

    update

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-const express = require('express')
-const app = express()
-const http = require('http').Server(app)
-const cors = require('cors')
+import cors from 'cors'
+import express from 'express'
+import http from 'http'
+import request from 'request'
+
 const PORT = process.env.PORT | 8080
+const app = express()
+const server = http.createServer(app)
 
-const request = require('request')
-
-app.use(express.static(`${__dirname}dist/`))
 app.use(cors())
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
+app.use(express.json())
 
 app.get('/test/*', function (req, res) {
   request(req.url.substring(6), function (error, response, body) {
@@ -25,4 +26,6 @@ app.get('*', function (req, res) {
   res.send('coucou')
 })
 
-http.listen(PORT, () => { console.log(`listening on *:${PORT}`) })
+server.listen(PORT, () => {
+  console.log('listening on *:' + PORT)
+})
